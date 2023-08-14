94efb067db2c912b0a67f56dfeffbee75fe76e47
blockdev <1689531+0xbok@users.noreply.github.com>
2023-08-14 22:34:03 +0400

    send mint event to queue (#11)

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use ethers::prelude::*;
 
 use axum::{extract::Extension, routing::get, Router, Server};
 use lapin::Channel;
+use serde::{Deserialize, Serialize};
 use tokio_postgres::NoTls;
 use user_balance::UserBalanceConfig;
 
-use model::MutationRoot;
+use model::{MutationRoot, SendMessageType};
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
@@ -47,61 +48,70 @@ enum MerkleCommand {
 struct ApiContext {
     user_balance_db: UserBalanceConfig,
     mt: Arc<RwLock<MerkleTree<PostgresDBConfig, MyPoseidon>>>,
-    channel: Channel,
+    channel: Arc<Channel>,
 }
 
 const MERKLE_DEPTH: usize = 32; // TODO: read in from parameters file
 const ARCPAY_ADDRESS: &str = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+
+const QUEUE_NAME: &str = "user_requests";
+
 abigen!(ArcPayContract, "ArcPay.json");
+
+#[derive(Debug, Serialize, Deserialize)]
+enum QueueMessage {
+    Mint { receiver: H160, amount: U256 },
+    Send(SendMessageType),
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //////////////// experimental RabbitMQ integration ////////////////
+    // Connect to the RabbitMQ server
+    let addr = "amqp://guest:guest@localhost:5672/%2f";
+    let conn = Connection::connect(addr, ConnectionProperties::default()).await?;
+
+    // Create a channel
+    let channel = conn.create_channel().await?;
+
+    // TODO: declare queue as persistent through `QueueDeclareOptions`. See message durability section:
+    // https://www.rabbitmq.com/tutorials/tutorial-two-python.html.
+    // Also make messages persistent by updating publisher. The value `PERSISTENT_DELIVERY_MODE` is 2,
+    // see: https://pika.readthedocs.io/en/stable/_modules/pika/spec.html.
+    // Even this doesn't fully guarantee message persistence, see "Note on message persistence"
+    // in the tutorial which links to: https://www.rabbitmq.com/confirms.html#publisher-confirms.
+    // We have enabled publisher confirms in the next step.
+    channel
+        .queue_declare(
+            QUEUE_NAME,
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+    // Enable publisher confirms
+    channel
+        .confirm_select(ConfirmSelectOptions::default())
+        .await?;
+
+    // Consume messages from the queue
+    let consumer = channel
+        .basic_consume(
+            QUEUE_NAME,
+            "my_consumer",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .expect("basic_consume");
+
+    println!("Waiting for messages...");
+    tokio::spawn(async move { send_consumer::send_consumer(consumer).await });
+
     let cli = Cli::parse();
+    let mut handles = vec![];
+    let channel = Arc::new(channel);
 
     if let Some(t) = cli.merkle {
-        //////////////// experimental RabbitMQ integration ////////////////
-        // Connect to the RabbitMQ server
-        let addr = "amqp://guest:guest@localhost:5672/%2f";
-        let conn = Connection::connect(addr, ConnectionProperties::default()).await?;
-
-        // Create a channel
-        let channel = conn.create_channel().await?;
-
-        // Declare a queue
-        let queue_name = "send_request_queue";
-
-        // TODO: declare queue as persistent through `QueueDeclareOptions`. See message durability section:
-        // https://www.rabbitmq.com/tutorials/tutorial-two-python.html.
-        // Also make messages persistent by updating publisher. The value `PERSISTENT_DELIVERY_MODE` is 2,
-        // see: https://pika.readthedocs.io/en/stable/_modules/pika/spec.html.
-        // Even this doesn't fully guarantee message persistence, see "Note on message persistence"
-        // in the tutorial which links to: https://www.rabbitmq.com/confirms.html#publisher-confirms.
-        // We have enabled publisher confirms in the next step.
-        channel
-            .queue_declare(
-                queue_name,
-                QueueDeclareOptions::default(),
-                FieldTable::default(),
-            )
-            .await?;
-        // Enable publisher confirms
-        channel
-            .confirm_select(ConfirmSelectOptions::default())
-            .await?;
-
-        // Consume messages from the queue
-        let consumer = channel
-            .basic_consume(
-                queue_name,
-                "my_consumer",
-                BasicConsumeOptions::default(),
-                FieldTable::default(),
-            )
-            .await
-            .expect("basic_consume");
-
-        println!("Waiting for messages...");
-
         //////////////// experimental GraphQL integration /////////////////
         let (client, connection) =
             tokio_postgres::connect("host=localhost user=dev dbname=arcpay", NoTls).await?;
@@ -131,8 +141,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         };
 
-        tokio::spawn(async move { send_consumer::send_consumer(consumer, t).await });
-
         let user_balance_db = UserBalanceConfig {
             client,
             tablename: "user_balance".to_string(),
@@ -142,16 +150,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .data(ApiContext {
                 user_balance_db,
                 mt: Arc::new(RwLock::new(mt)),
-                channel,
+                channel: channel.clone(),
             })
             .finish();
         let app = Router::new()
             .route("/", get(graphql_playground).post(graphql_handler))
             .route("/health", get(health))
             .layer(Extension(schema));
-        Server::bind(&"0.0.0.0:8000".parse()?)
-            .serve(app.into_make_service())
-            .await?;
+        handles.push(tokio::spawn(async move {
+            Server::bind(&"0.0.0.0:8000".parse().unwrap())
+                .serve(app.into_make_service())
+                .await
+                .unwrap()
+        }));
     }
 
     if cli.mint {
@@ -171,8 +182,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let event = contract.event_for_name("Mint")?;
         let filter = event.filter.from_block(0).to_block(BlockNumber::Finalized);
         */
-        let mint_task = tokio::spawn(mint(events));
-        mint_task.await?;
+        handles.push(tokio::spawn(
+            async move { mint(events, channel.clone()).await },
+        ));
+    }
+
+    for handle in handles {
+        handle.await.expect("joined task panicked");
     }
 
     Ok(())
